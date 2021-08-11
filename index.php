c58d6140a8d2f5b34d0d2e8e95af19fd542f28d3
Deák Ruben <rubendeak15@gmail.com>
2021-08-11 18:59:25 +0300

    Create a front page and a login page

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
-<?php
-    // Initialize the session
-    session_start();
-
-    // Define variables and initialize with empty values
-    $username = $password = "";
-    $usernameErr = $passwordErr = $loginErr = "";
-
-    // Processing form data when form is submitted
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-        // Check if username is empty
-        if(empty(trim($_POST['username']))){
-            $usernameErr = "Please enter your username.";
-        }else{
-            $username = $_POST['username'];
-            $_SESSION["username"] = $username;
-        }
-
-        // Check if password is empty
-        if(empty(trim($_POST['password']))){
-            $passwordErr = "Please enter your password.";
-        }else{
-            $password = $_POST['password'];
-        }
-
-        // TEMPORARY WHILE NO HAVE DATABASE CONNECTION
-        if(isset($_POST['username']) && isset($_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])){
-            header("location: welcome.php");
-            exit;
-        }
-
-    }
-
-?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -44,21 +9,16 @@
 </head>
 <body>
     <div>
-        <h2>Login</h2>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <div>
-                <label>Username</label>
-                <input type="text" name="username" required />
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" name="password" required />
-            </div>
-            <div>
-                <input type="submit" name="submitBtn" value="Login" />
-            </div>
+        <h1>Front Website</h1>
+            <p><b><a href="login.php">Login</a></b></p>
             <p>Don't have an account? <a href="">Sign up now</a>.</p>
-        </form>
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce risus sem, fringilla id felis vitae, condimentum faucibus leo. Aliquam varius massa ac condimentum rhoncus. Proin eleifend dolor augue, id fermentum nunc viverra imperdiet. Nullam dignissim vel nulla vel tincidunt. Suspendisse potenti. Pellentesque malesuada ut est nec aliquam. Praesent nec porta odio. Praesent odio nibh, placerat in volutpat nec, finibus eget leo. Sed fermentum orci ut rutrum fringilla. Donec felis odio, accumsan luctus pellentesque sit amet, scelerisque viverra diam. Phasellus euismod consequat risus, ac finibus nisi. Nulla a lacus velit. Ut lobortis purus ut nisl dapibus, non ultricies quam scelerisque.
+        Fusce pretium fringilla felis, sit amet hendrerit magna molestie et. Duis nec imperdiet arcu, et elementum lorem. Suspendisse sapien mi, finibus vitae odio nec, ultricies efficitur felis. Quisque sodales vehicula congue. Morbi eleifend, orci eget condimentum volutpat, nunc libero iaculis nibh, sed pulvinar lacus leo in diam. Phasellus quis maximus purus, non maximus elit. Sed quis felis consequat, mollis urna et, semper nulla. Donec venenatis, massa nec efficitur commodo, orci quam aliquet eros, quis tempor enim eros at purus. Suspendisse non sapien auctor, elementum lectus ut, lacinia leo.
+        Curabitur elementum felis justo, ut accumsan neque luctus vitae. Cras tellus quam, interdum eu commodo vitae, gravida nec magna. Proin aliquet magna ut massa sagittis, nec fermentum mi imperdiet. Curabitur sed tellus sed ligula dignissim gravida. Cras porta a orci vel pulvinar. Proin sodales mollis fringilla. Duis ac sem ut nulla dignissim condimentum. Nam scelerisque blandit ligula, a ornare mi sollicitudin eu. Pellentesque ut tempor diam. Nam tincidunt velit lectus, euismod consequat justo rutrum tempor. Phasellus ultrices, leo nec convallis varius, sem sem volutpat lectus, et elementum orci urna aliquam velit. Morbi feugiat mattis aliquam.
+        Quisque blandit pharetra quam, at malesuada dui pulvinar non. Aliquam a enim quis justo dapibus tristique. Donec vitae elit rhoncus, tempor velit et, ultrices nisl. Nam sed purus pulvinar, dapibus tellus lacinia, fermentum urna. Pellentesque tristique viverra nisl sit amet fringilla. Donec sit amet erat non dui placerat pharetra. Maecenas dolor ex, fermentum id sodales sed, sodales in nulla. Vestibulum ut purus tortor. Sed sollicitudin commodo enim, at tempor est lobortis id. Sed iaculis molestie ex, eu ornare ligula. Sed sit amet tincidunt quam. Nulla facilisi. Suspendisse id pharetra erat, et rutrum odio. Sed blandit leo vel dui aliquet tincidunt. Maecenas ac sagittis mi. Phasellus porttitor, augue sed ultrices consequat, leo dolor rhoncus nisi, et ornare metus urna sed nulla.
+        Donec orci ante, porta eu egestas ut, sollicitudin et urna. Nullam sollicitudin arcu sed ex imperdiet tincidunt. Sed venenatis condimentum ligula sed hendrerit. In viverra lectus et ante rhoncus, vulputate pellentesque nisi mollis. Proin feugiat ipsum vitae mauris mattis vestibulum. Sed eleifend ullamcorper ante, vitae convallis massa maximus sit amet. Sed odio eros, volutpat vel elementum vulputate, gravida sit amet orci.
     </div>
 </body>
 </html>
